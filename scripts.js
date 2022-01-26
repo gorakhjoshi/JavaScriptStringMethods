af3ae2603bc0827dae80dab8025ce7c262913bb3
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-26 13:42:50 +0545

    string as an array

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const airline = 'Nepal Airlines';
+const plane = 'NA540';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('NA143'[0]);
+
+console.log(airline.length);
+console.log('NA143'.length);
