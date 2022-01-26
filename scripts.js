2d483624d156b0ceeb7c154ada6d53125d802254
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-26 13:48:53 +0545

    practical example with slice method

--- scripts.js
+++ scripts.js
@@ -21,3 +21,14 @@ console.log(airline.slice(airline.lastIndexOf(' ') + 1));
 
 console.log(airline.slice(-2));
 console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
