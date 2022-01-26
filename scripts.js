e4606c8c5fd60830d4b2af9e4e48bf03937a4efd
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-26 13:46:31 +0545

    indexOf and lastIndexOf with slice

--- scripts.js
+++ scripts.js
@@ -12,3 +12,12 @@ console.log('NA143'.length);
 console.log(airline.indexOf('l'));
 console.log(airline.lastIndexOf('l'));
 console.log(airline.indexOf('Airlines'));
+
+console.log(airline.slice(6));
+console.log(airline.slice(6, 10));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
