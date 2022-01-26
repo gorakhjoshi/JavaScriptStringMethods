8727f2e86ccc65787801d22c61e3eb22bc9e9a9e
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-26 13:58:58 +0545

    practical example with includes

--- scripts.js
+++ scripts.js
@@ -75,3 +75,16 @@ console.log(newPlane.startsWith('Airb'));
 if (newPlane.startsWith('Airbus') && newPlane.endsWith('neo')) {
   console.log('Part of the NEW ARirbus family');
 }
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
