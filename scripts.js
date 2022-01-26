9ee2b468322299c1a9866411853d7c49ac771265
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-01-26 14:04:03 +0545

    practical example for capitalizing

--- scripts.js
+++ scripts.js
@@ -97,3 +97,15 @@ const newName = ['Mr.', firstName, middleName, lastName.toUpperCase()].join(
   ' '
 );
 console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('jessica ann smith davis');
+capitalizeName('gorakh raj joshi');
